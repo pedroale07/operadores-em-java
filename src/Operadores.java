639847576b205b6a9834bb238a2bf93bd10745b9
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-21 15:05:25 -0300

    redefinindo classe e fazendo configuracoes

--- src/Operadores.java
+++ src/Operadores.java
@@ -42,6 +42,21 @@ public class Operadores {
 		
 		OperadoresAritmericos aritmeticos = new OperadoresAritmericos();
 	
-			
+	//OPERADORES UNÁRIOS
+		/**
+		 * 
+		 * Esses operadores, são aplicados juntamente com um outro operador aritmético. Eles realizam alguns trabalhos básicos como incrementar, decrementar, inverter valores numéricos e booleanos.
+		 *	(+) Operador unário de valor positivo – números são positivos sem esse operador explicitamente;
+		 *	(-) Operador unário de valor negativo – nega um número ou expressão aritmética;
+		 *	(++) Operador unário de incremento de valor – incrementa o valor em 1 unidade;
+		 *	(--) Operador unário de decremento de valor – decrementa o valor em 1 unidade;
+		 *	(!) Operador unário lógico de negação – nega o valor de uma expressão booleana.
+		 *
+		 */
+	
+				/**
+			 	* adentre a classe abaixo para pode ver exemplos e como utiliza-la
+			 	*/
+		OperadoresUnarios unarios = new OperadoresUnarios(); 
 	}
 }
