822cfa544453f24c2d5a103477cb851899d2312c
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-21 14:23:26 -0300

    atualizando a classe principal e criando uma nova classe

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,10 +1,35 @@
 
 public class Operadores {
 	
+	
+	public static void main(String[] args) {
+		
+	
+	
 	/**
 	 * Os operadores são simbolos especiais que são chave dentro da plataforma
 	 * ele determinar comparações e algumas outras ações a serem feitas que veremos
 	 * mais abaixo em cada tópico
 	 */
 	
+		//CLASSIFICACAO DOS OPERADORES
+		/**
+		 * iremos abaixo comentar e descrever cada tipo de operador e como ele
+		 * funciona dentro do codigo
+		 */
+	
+	//OPERADORES DE ATRIBUICAO
+	/**
+	 * O operador de atribuicao e utilizado para definir o valor inicial ou sobrescrever
+	 * o valor de uma variavel em java, primeiro definimos um tipo a essa variavel e apos isso
+	 *iremos colocar o sinal de atribuicao e algum conteudo dentro como no exemplo abaixo 
+	 */
+	
+		/**
+		 * adentre a classe abaixo para pode ver exemplos e como utiliza-la
+		 */
+		OperadoresDeAtribuicao atribuicao = new OperadoresDeAtribuicao();
+	
+	
+	}
 }
