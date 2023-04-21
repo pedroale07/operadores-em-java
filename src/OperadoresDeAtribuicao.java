ea59a2b28abb147aaddcd38421bf6957946ce64f
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-21 14:34:26 -0300

    Adicionando mais alteracoes a classe principal

--- src/OperadoresDeAtribuicao.java
+++ src/OperadoresDeAtribuicao.java
@@ -1,4 +1,43 @@
 
 public class OperadoresDeAtribuicao {
+	
+	/**
+	 * nessa classe iremos ver exemplos de operadores com atribuicao
+	 */
+	
+	String nome = "Pedro Alessandro";
+	/**
+	 * note como esta a formacao acima. Usamos a seguinte formacao abaixo
+	 * 
+	 * <TipoDaVariavel> nomeDaVariavel = <CONTEUDO>
+	 * 
+	 * Note que o sinal que usamos para atribui e "=", ou seja, a variavel esta recebendo
+	 * um conteudo
+	 * 
+	 */
+	
+		/**
+		 * exemplos com numeros inteiros
+		 */
+		int idade = 20;
+
+		/**
+		 * exemplos com numeros decimais ou ponto flutuante
+		 */
+			double peso = 68.5;
+			
+		/**
+		 * tipo char que recebe apenas 1 caractere
+		 */
+
+			char sexo = 'M';
+			
+		/**
+		 * tipo boolean que cita sobre afirmacoes positivas ou negativas, esse tipo pode ter duas atribuicoes
+		 * ou ele pode ser positivo(true) ou pode ser negativo(false)
+		 */
+			boolean EuSouBonito = false;
+
+
 
 }
