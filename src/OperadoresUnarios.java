29103ae36bbb7eb783fdf227008df8023aa5a8ed
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-21 15:18:15 -0300

    Fazendo novas alteracoes na classe

--- src/OperadoresUnarios.java
+++ src/OperadoresUnarios.java
@@ -1,4 +1,31 @@
 
 public class OperadoresUnarios {
+	
+	//exemplos de operadores unarios abaixo
+	
+	public static void main(String[] args) {
+		
+		int numero = 10;
+		
+		//imprimindo a numero negativo
+		System.out.println(-numero);
+		
+		//imcrementando um numero mais 1, imprime 11
+		numero++;
+		System.out.println(numero);
+		
+		//imcrementando um numero mais 1, imprime 11
+		System.out.println(numero++);
+			
+		//agora o numero voltou ao valor inicial
+		System.out.println(numero);
+		
+		//ordem de precedencia conta aqui
+		System.out.println(++ numero);
+
+		boolean verdadeiro = true;
+
+		System.out.println("Inverteu " + !verdadeiro);
+	}
 
 }
