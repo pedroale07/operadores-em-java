808e15589f2609a48910f4511b0ab29d7188c0f1
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-21 15:23:03 -0300

    alteracoes e atualizacoes em classe primarias

--- src/Operadores.java
+++ src/Operadores.java
@@ -58,5 +58,18 @@ public class Operadores {
 			 	* adentre a classe abaixo para pode ver exemplos e como utiliza-la
 			 	*/
 		OperadoresUnarios unarios = new OperadoresUnarios(); 
+		
+		
+		
+		//OPERADORES TERNARIOS
+			/**
+			 * 
+			 *  O operador de condição ternária é uma forma resumida para definir uma condição e escolher por um dentre dois valores. Você deve pensar numa condição ternária, como se fosse uma condição IF normal, porém, de uma forma em que toda a sua estrutura estará escrita numa única linha.
+			 *  O operador ternário é representado pelos símbolos "?" e ":" utilizados na seguinte estrutura de sintaxe:
+			 *	<Expressão Condicional>`` ?``<Caso condição seja true>``: ``<Caso condição seja false>
+			 * 
+			 */
+		
+		OperadoresTernarios ternarios = new OperadoresTernarios();
 	}
 }
