ffedabd45ceb02fb0e931d4a2037bfe3988b521e
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-21 14:13:06 -0300

    Criando classe principal do projeto

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,6 +1,10 @@
 
 public class Operadores {
 	
+	/**
+	 * Os operadores são simbolos especiais que são chave dentro da plataforma
+	 * ele determinar comparações e algumas outras ações a serem feitas que veremos
+	 * mais abaixo em cada tópico
+	 */
 	
-
 }
