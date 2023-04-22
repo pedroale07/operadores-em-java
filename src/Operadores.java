3c168e7f4232f9658ed65732caab7ae5ebe0bec3
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-22 20:27:01 -0300

    atualizando alteracoes

--- src/Operadores.java
+++ src/Operadores.java
@@ -75,5 +75,22 @@ public class Operadores {
 					*/
 		
 		OperadoresTernarios ternarios = new OperadoresTernarios();
+		
+		
+		//OPERADORES RELACIONAIS
+			/**
+			 * 
+			 *  Os operadores relacionais, avaliam a relação entre duas variáveis ou expressões. Neste caso, mais precisamente, definem se o operando à esquerda é igual, diferente, menor, menor ou igual, maior, maior ou igual ao da direita, retornando um valor booleano como resultado.
+			 *	== Quando desejamos verificar se uma variável é IGUAL A outra.
+			 *	!= Quando desejamos verificar se uma variável é DIFERENTE da outra.
+			 *	> Quando desejamos verificar se uma variável é MAIOR QUE a outra.
+			 *	>= Quando desejamos verificar se uma variável é MAIOR OU IGUAL a outra.
+			 *	< Quando desejamos verificar se uma variável é MENOR QUE outra.
+			 *	<= Quando desejamos verificar se uma variável é MENOR OU IGUAL a outra.
+			 * 
+			 */
+		
+			OperadoresRelacionais relacionais = new OperadoresRelacionais();
+			
 	}
 }
