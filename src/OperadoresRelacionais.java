4baf5a91289d31d8dc9a1a27c08ea8a6d8dd99cd
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-22 20:54:01 -0300

    enviando atualizacoes

--- src/OperadoresRelacionais.java
+++ src/OperadoresRelacionais.java
@@ -1,4 +1,25 @@
 
 public class OperadoresRelacionais {
+	
+	public static void main(String[] args) {
+		
+		int numero1 = 1;
+		int numero2 = 2;
+		
+		if(numero1 > numero2) 
+			System.out.println("numero 1 maior que numero 2");
+		
+		if(numero1 < numero2)
+			System.out.println("numero 1 menor que numero 2");
+		
+		if(numero1>=numero2)
+			System.out.println("numero 1 maior ou igual a numero 2");
+	
+		if(numero1 <=numero2)
+			System.out.println("numero 1 maior ou igual ao numero 2");
+	
+		if(numero1!=numero2)
+			System.out.println("Numero 1 diferente de numero 2");
+	}
 
 }
