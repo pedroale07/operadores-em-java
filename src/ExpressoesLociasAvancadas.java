0bdc7f5ef801e1a52a834aff89fee6c69a9c1599
Pedro Alessandro <pedro.paz13@hotmail.com>
2023-04-22 21:41:42 -0300

    adicionando mais uma classe ao nosso projeto

---
+++ src/ExpressoesLociasAvancadas.java
@@ -0,0 +1,18 @@
+
+public class ExpressoesLociasAvancadas {
+	
+	public static void main(String[] args) {
+		
+		int numero1 = 1;
+		int numero2 = 1;
+		
+		if(numero1==2 & numero2 ++ == 2)
+			System.out.println("As duas condicoes sao verdadeiras");
+		
+			System.out.println("O numero 1 agora é: "+numero1);
+			System.out.println("O numero 2 agora é: "+numero2);
+			
+			
+	}
+
+}
